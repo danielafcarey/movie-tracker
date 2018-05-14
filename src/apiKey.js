03600ef2e73901f2a7754d41e87a98935872875a
Daniela Carey <danielafcarey@gmail.com>
2018-05-14 16:24:16 -0600

    Basic file setup and directory organization
    Setup for redux and router

---
+++ src/apiKey.js
@@ -0,0 +1,1 @@
+export default apiKey = 'b6aef20ba0585e52cb5f73c24b49fad6';
--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import App from './containers/App/App';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from '../src/reducers';
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = createStore(rootReducer, devTools);
 
 const router = (
-  <Router>
-    <App />  
-  </Router>
-)
+  <Provider store={ store }>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+ )
 
 ReactDOM.render(router, document.getElementById('root'));
