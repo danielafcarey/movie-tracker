7eaaf31d2a016baa0acf299d46d02e8fd71cf50b
Daniela Carey <danielafcarey@gmail.com>
2018-05-20 12:43:55 -0600

    Move Card from components to containers

--- src/containers/Movies/Movies.js
+++ src/containers/Movies/Movies.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-import Card from '../../components/Card/Card';
+import Card from '../Card/Card';
 
 const Movies = ({ movies }) => {
   const movieCards = movies.map(movie => {
