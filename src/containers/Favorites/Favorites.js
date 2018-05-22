0ab39af01536057edaae2a82dcce2496cb0f4b41
Daniela Carey <danielafcarey@gmail.com>
2018-05-21 19:36:13 -0600

    Clean up Favorites component
    Add semicolons
    Remove unused vars

--- src/containers/Favorites/Favorites.js
+++ src/containers/Favorites/Favorites.js
@@ -1,30 +1,30 @@
 import React from 'react';
 import Card from '../Card/Card';
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
 
 const Favorites = (props) => {
   if (!props.favorites) {
-    return <div>Get a job!</div>
+    return <div>Get a job!</div>;
   }
+
   const favoriteCards = props.favorites.map(favorite => {
-    return <Card { ...favorite } key={ favorite.movieId } />
-  }) 
+    return <Card { ...favorite } key={ favorite.movieId } />;
+  }); 
 
   return (
     <div className='favorites-container'>
       { favoriteCards } 
     </div>
-  )
-}
+  );
+};
 
 const mapStateToProps = (state) => ({
   favorites: state.favorites
-})
+});
 
 export {
   Favorites,
   mapStateToProps
-}
+};
 
 export default connect(mapStateToProps)(Favorites);
